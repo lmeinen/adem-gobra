afa7a2bac16568aedeb448619773d671e1bdfd0b
Lasse_Meinen <lmeinen@ethz.ch>
2023-10-11 13:30:02 +0200

    backup commit of goification of gobra files

--- pkg/util/b64.go
+++ pkg/util/b64.go
@@ -1,15 +1,24 @@
 package util
 
-import "encoding/base64"
+import (
+	"encoding/base64"
+	//@ p "predicates"
+)
 
 // B64Dec decodes a base64-encoded string (represented as byte array) into a
 // byte array.
-func B64Dec(src []byte) ([]byte, error) {
+// @ preserves p.BytesMem(src)
+// @ ensures err == nil ==> p.BytesMem(res)
+func B64Dec(src []byte) (res []byte, err error) {
 	dst := make([]byte, base64.StdEncoding.DecodedLen(len(src)))
+	//@ fold p.BytesMem(dst)
 	n, err := base64.StdEncoding.Decode(dst, src)
+	//@ unfold p.BytesMem(dst)
 	if err != nil {
 		return nil, err
 	} else {
-		return dst[:n], nil
+		res := dst[:n]
+		//@ fold p.BytesMem(res)
+		return res, nil
 	}
 }
