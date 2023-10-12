fdd332aa893127ae50fcbce348d497d8961f9ef6
Lasse_Meinen <lmeinen@ethz.ch>
2023-10-12 14:13:47 +0200

    Moved Gobra code directly into .go files

--- pkg/util/set.go
+++ pkg/util/set.go
@@ -1,50 +1,119 @@
 package util
 
-type Elem[T comparable] interface {
-	ToComparable() T
+type Elem interface {
+	// @ pred mem()
+
+	// @ requires acc(mem(), _)
+	// @ pure
+	ToComparable() (res int64)
 }
 
-type Set[T comparable] interface {
-	Has(Elem[T]) Elem[T]
-	HasKey(T) Elem[T]
-	Add(Elem[T])
-	Rm(Elem[T]) Elem[T]
-	Size() int
+type Set interface {
+	// @ pred mem()
+
+	// @ preserves p0 > 0 && p1 > 0 && acc(mem(), p0) && acc(val.mem(), p1)
+	Has(val Elem /*@, ghost p0, p1 perm @*/) (res Elem)
+
+	// @ preserves p > 0 && acc(mem(), p)
+	HasKey(key int64 /*@, ghost p perm @*/) (res Elem)
+
+	// @ preserves mem() && p > 0 && acc(val.mem(), p)
+	Add(val Elem /*@, ghost p perm @*/)
+
+	// @ preserves mem() && p > 0 && acc(val.mem(), p)
+	Rm(val Elem /*@, ghost p perm @*/) (res Elem)
+
+	// @ requires p > 0 && acc(mem(), p)
+	// @ ensures 0 <= res
+	// @ pure
+	Size( /*@ ghost p perm @*/ ) (res int)
 }
 
-type set[T comparable] map[T]Elem[T]
+type int64Set map[int64]Elem
 
-func MkSet[T comparable]() Set[T] {
-	m := make(set[T])
-	return &m
+func MkSet() Set {
+	i64Set := make(int64Set) // @ addressable: i64Set
+	return &i64Set
 }
 
-func (s *set[T]) Has(val Elem[T]) Elem[T] {
+// @ preserves p0 > 0 && p1 > 0 && acc(s, p0) && acc(*s, p0) && acc(val.mem(), p1)
+func (s *int64Set) Has(val Elem /*@, ghost p0, p1 perm @*/) (res Elem) {
 	if val != nil {
 		return (*s)[val.ToComparable()]
 	}
 	return nil
 }
 
-func (s *set[T]) HasKey(val T) Elem[T] {
-	return (*s)[val]
+// @ preserves p > 0 && acc(s, p) && acc(*s, p)
+func (s *int64Set) HasKey(key int64 /*@, ghost p perm @*/) (res Elem) {
+	return (*s)[key]
 }
 
-func (s *set[T]) Add(val Elem[T]) {
+// @ preserves acc(s) && acc(*s) && p > 0 && acc(val.mem(), p)
+func (s *int64Set) Add(val Elem /*@, ghost p perm @*/) {
 	if val != nil {
 		(*s)[val.ToComparable()] = val
 	}
 }
 
-func (s *set[T]) Rm(val Elem[T]) Elem[T] {
+// @ preserves acc(s) && acc(*s) && p > 0 && acc(val.mem(), p)
+func (s *int64Set) Rm(val Elem /*@, ghost p perm @*/) (res Elem) {
 	if val != nil {
 		e := (*s)[val.ToComparable()]
-		delete(*s, val.ToComparable())
+		s.DoDeleteElem(val.ToComparable())
 		return e
 	}
 	return nil
 }
 
-func (s *set[T]) Size() int {
+// @ pure
+// @ requires p > 0 && acc(s, p) && acc(*s, p)
+// @ ensures 0 <= res
+func (s *int64Set) Size( /*@ ghost p perm @*/ ) (res int) {
 	return len(*s)
 }
+
+// @ trusted
+// @ preserves acc(s) && acc(*s)
+func (s *int64Set) DoDeleteElem(key int64) {
+	delete((*s), key)
+}
+
+/*@
+pred (s *int64Set) mem() {
+  acc(s) && acc(*s)
+}
+@*/
+
+/*@
+(*int64Set) implements Set{
+
+  (s *int64Set) Has(val Elem, ghost p0 perm, ghost p1 perm) (res Elem) {
+	unfold acc(s.mem(), p0)
+	res = s.Has(val, p0, p1)
+	fold acc(s.mem(), p0)
+  }
+
+  (s *int64Set) HasKey(key int64, ghost p perm) (res Elem) {
+	unfold acc(s.mem(), p)
+	res = s.HasKey(key, p)
+	fold acc(s.mem(), p)
+  }
+
+  (s *int64Set) Add(val Elem, ghost p perm) () {
+	unfold s.mem()
+	s.Add(val, p)
+	fold s.mem()
+  }
+
+  (s *int64Set) Rm(val Elem, ghost p perm) (res Elem) {
+	unfold s.mem()
+	res = s.Rm(val, p)
+	fold s.mem()
+  }
+
+  pure (s *int64Set) Size(ghost p perm) (int) {
+	return unfolding acc(s.mem(), p) in s.Size(p)
+  }
+}
+@*/
--- pkg/util/slice.go
+++ pkg/util/slice.go
@@ -1,10 +1,20 @@
 package util
 
-func Contains[T comparable](slice []T, v T) bool {
-	for _, elem := range slice {
-		if elem == v {
-			return true
+// @ requires pSlice > 0 && pSliceElem > 0
+// @ requires isComparable(v)
+// @ requires forall k int ::  0 <= k && k < len(slice) ==> acc(&slice[k], pSliceElem)
+// @ requires forall k int ::  0 <= k && k < len(slice) ==> isComparable(slice[k]) && typeOf(slice[k]) == typeOf(v)
+// @ preserves acc(slice, pSlice)
+// @ ensures forall k int ::  0 <= k && k < len(slice) ==> acc(&slice[k], pSliceElem)
+// @ ensures res ==> 0 <= idx && idx < len(slice) && slice[idx] == v
+func Contains(slice []interface{}, v interface{} /*@, ghost pSlice, pSliceElem perm @*/) (res bool /*@, ghost idx int @*/) {
+	//@ invariant acc(slice, pSlice)
+	for i, elem := range slice {
+		{
+			if elem == v {
+				return true //@ with: i
+			}
 		}
 	}
-	return false
+	return false //@ with: 0 - 1
 }
