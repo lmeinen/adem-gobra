3468629579414168d84dbd30baab2200fbce0a8b
Lasse_Meinen <lmeinen@ethz.ch>
2023-10-20 13:42:56 +0200

    reduced util pkg to the bare necessity for Gobra not to throw errors (backup in wip package)

--- pkg/util/promises.go
+++ pkg/util/promises.go
@@ -4,35 +4,12 @@ package util
 
 import jwk "github.com/lestrrat-go/jwx/v2/jwk"
 
-/*@
-type PromiseState adt {
-	US{} // 0 Unfulfilled State
-	FS{} // 1 Fulfilled State
-	RS{} // 2 Rejected State
-	GS{} // 3 Gotten State
-}
-@*/
-
 type Promise interface {
-	// @ pred mem()
-
-	// @ requires mem() && GetState() == US{}
-	// @ ensures mem() && GetState() == FS{}
 	Fulfill(jwk.Key)
 
-	// @ requires mem() && GetState() == US{}
-	// @ ensures mem() && GetState() == RS{}
 	Reject()
 
-	// @ requires mem()
-	// @ ensures mem() && GetState() == GS{}
 	Get() (res jwk.Key)
-
-	/*@
-	  ghost
-	  requires mem()
-	  pure GetState() (PromiseState)
-	  @*/
 }
 
 type promise struct {
@@ -41,56 +18,42 @@ type promise struct {
 }
 
 // Create a new promise.
-// @ ensures res != nil && res.mem() && res.GetState() == US{}
+
 func NewPromise() (res Promise) {
-	newProm := promise{ch: make(chan jwk.Key, 1), state: 0} // @ addressable: newProm
+	newProm /*@@@*/ := promise{ch: make(chan jwk.Key, 1), state: 0}
 	// @ newProm.ch.Init(SendInvariant!<_!>, PredTrue!<!>)
 	// @ newProm.ch.CreateDebt(1, 2, PredTrue!<!>)
-	// @ fold newProm.mem()
 	return &newProm
 }
 
-// @ requires p.mem() && p.GetState() == US{}
-// @ ensures p.mem() && p.GetState() == FS{}
 func (p *promise) Fulfill(val jwk.Key) {
-	// @ unfold p.mem()
 	// @ fold SendInvariant!<_!>(val)
 	p.ch <- val
 	// @ fold PredTrue!<!>()
-	close(p.ch) /*@ with: 1, 2, PredTrue!<!> @*/
+	close(p.ch /*@, 1, 2, PredTrue!<!> @*/)
 	p.state = 1
-	// @ fold p.mem()
+
 }
 
-// @ requires p.mem() && p.GetState() == US { }
-// @ ensures p.mem() && p.GetState() == RS { }
 func (p *promise) Reject() {
-	// @ unfold p.mem()
 	// @ fold PredTrue!<!>()
-	close(p.ch) /*@ with: 1, 2, PredTrue!<!> @*/
+	close(p.ch /*@, 1, 2, PredTrue!<!> @*/)
 	p.state = 2
-	// @ fold p.mem()
 }
 
-// @ requires p.mem()
-// @ ensures p.mem() && p.GetState() == GS { }
 func (p *promise) Get() (res jwk.Key) {
-	// @ unfold p.mem()
 	// @ fold PredTrue!<!>()
 	e := <-p.ch
 	p.state = 3
-	// @ fold p.mem()
 	return e
 }
 
-// @ ensures res != nil && res.mem() && res.GetState() == FS { }
 func Fullfilled(val jwk.Key) (res Promise) {
 	p := NewPromise()
 	p.Fulfill(val)
 	return p
 }
 
-// @ ensures res != nil && res.mem() && res.GetState() == RS { }
 func Rejected() (res Promise) {
 	p := NewPromise()
 	p.Reject()
@@ -98,38 +61,7 @@ func Rejected() (res Promise) {
 }
 
 /*@
-ghost
-requires p.mem()
-pure func (p *promise) GetState() (PromiseState) {
-  return unfolding p.mem() in match p.state {
-    case 0: US{}
-    case 1: FS{}
-    case 2: RS{}
-    case 3: GS{}
-  }
-}
-@*/
-
-/*@
-pred (p *promise) mem() {
-	acc(p) && 0 <= p.state && p.state <= 3 &&
-	p.ch.RecvChannel() && p.ch.Token(PredTrue!<!>) &&
-	p.ch.RecvGivenPerm() == PredTrue!<!> &&
-	(p.state == 0 ==> acc(p.ch.SendChannel(), 1 / 2) &&
-		p.ch.ClosureDebt(PredTrue!<!>, 1, 2) &&
-		p.ch.SendGivenPerm() == SendInvariant!<_!>) && // not closed
-	(p.state == 1 || p.state == 2 ==> p.ch.Closed()) && // closed
-	// Note that we can't make any guarantees on the state of p.ch at the end of a Get call
-	(p.state == 3 ==> true)
-}
-@*/
-
-/*@
 pred SendInvariant(val jwk.Key) {
   true
 }
-@*/
-
-/*@
-*promise implements Promise
 @*/
--- pkg/util/slice.go
+++ pkg/util/slice.go
@@ -6,40 +6,24 @@ import (
 	"github.com/adem-wg/adem-proto/pkg/consts"
 )
 
-// @ preserves pSlice > 0 && pSliceElem > 0
-// @ preserves acc(slice, pSlice)
-// @ preserves forall k int ::  0 <= k && k < len(slice) ==> acc(&slice[k], pSliceElem)
-// @ ensures res ==> 0 <= idx && idx < len(slice) && slice[idx] == v
-func ContainsVerificationResult(slice []consts.VerificationResult, v consts.VerificationResult /*@, ghost pSlice, pSliceElem perm @*/) (res bool /*@, ghost idx int @*/) {
-	//@ invariant acc(slice, pSlice)
-	for i, elem := range slice {
+func ContainsVerificationResult(slice []consts.VerificationResult, v consts.VerificationResult) (res bool) {
+	for _, elem := range slice {
 		{
-			noop(i) // needed to avoid Go compiler error due to unused variable i
 			if elem == v {
-				return true //@ with: i
+				return true
 			}
 		}
 	}
-	return false //@ with: 0 - 1
+	return false
 }
 
-// @ preserves pSlice > 0 && pSliceElem > 0
-// @ preserves acc(slice, pSlice)
-// @ preserves forall k int ::  0 <= k && k < len(slice) ==> acc(&slice[k], pSliceElem)
-// @ ensures res ==> 0 <= idx && idx < len(slice) && slice[idx] == v
-func ContainsString(slice []string, v string /*@, ghost pSlice, pSliceElem perm @*/) (res bool /*@, ghost idx int @*/) {
-	// @ invariant acc(slice, pSlice)
-	for i, elem := range slice {
+func ContainsString(slice []string, v string) (res bool) {
+	for _, elem := range slice {
 		{
-			noop(i) // needed to avoid Go compiler error due to unused variable i
 			if elem == v {
-				return true //@ with: i
+				return true
 			}
 		}
 	}
-	return false //@ with: 0 - 1
-}
-
-func noop(_ int) {
-	// noop
+	return false
 }
